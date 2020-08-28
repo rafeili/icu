5427ca4c0c6ca5bc88a572da50fb51aba950339b
Shane F. Carr <shane@unicode.org>
2020-08-28 00:34:54 -0500

    Review feedback.

--- icu4c/source/i18n/unicode/nounit.h
+++ icu4c/source/i18n/unicode/nounit.h
@@ -29,17 +29,20 @@ U_NAMESPACE_BEGIN
 
 /**
  * Dimensionless unit for percent and permille.
+ * Prior to ICU 68, this namespace was a class with the same name.
  * @see NumberFormatter
- * @draft ICU 60
+ * @draft ICU 68
  */
 namespace NoUnit {
     /**
      * Returns an instance for the base unit (dimensionless and no scaling).
      *
+     * Prior to ICU 68, this function returned a NoUnit by value.
+     *
      * Since ICU 68, this function returns the same value as the default MeasureUnit constructor.
      *
      * @return               a MeasureUnit instance
-     * @draft ICU 60
+     * @draft ICU 68
      */
     static inline MeasureUnit U_EXPORT2 base() {
         return MeasureUnit();
@@ -48,10 +51,12 @@ namespace NoUnit {
     /**
      * Returns an instance for percent, or 1/100 of a base unit.
      *
+     * Prior to ICU 68, this function returned a NoUnit by value.
+     *
      * Since ICU 68, this function returns the same value as MeasureUnit::getPercent().
      *
      * @return               a MeasureUnit instance
-     * @draft ICU 60
+     * @draft ICU 68
      */
     static inline MeasureUnit U_EXPORT2 percent() {
         return MeasureUnit::getPercent();
@@ -60,10 +65,12 @@ namespace NoUnit {
     /**
      * Returns an instance for permille, or 1/1000 of a base unit.
      *
+     * Prior to ICU 68, this function returned a NoUnit by value.
+     *
      * Since ICU 68, this function returns the same value as MeasureUnit::getPermille().
      *
      * @return               a MeasureUnit instance
-     * @draft ICU 60
+     * @draft ICU 68
      */
     static inline MeasureUnit U_EXPORT2 permille() {
         return MeasureUnit::getPermille();
--- icu4j/main/classes/core/src/com/ibm/icu/util/NoUnit.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/NoUnit.java
@@ -7,16 +7,18 @@ import com.ibm.icu.number.NumberFormatter;
 /**
  * Dimensionless unit for percent and permille.
  * @see NumberFormatter
- * @draft ICU 60
+ * @draft ICU 68
  * @provisional This API might change or be removed in a future release.
  */
 public final class NoUnit {
     /**
      * Constant for the base unit (dimensionless and no scaling).
      *
+     * Prior to ICU 68, this constant equaled an instance of NoUnit.
+     *
      * Since ICU 68, this constant equals null.
      *
-     * @draft ICU 60
+     * @draft ICU 68
      * @provisional This API might change or be removed in a future release.
      */
     public static final MeasureUnit BASE = null;
@@ -24,9 +26,11 @@ public final class NoUnit {
     /**
      * Constant for the percent unit, or 1/100 of a base unit.
      *
+     * Prior to ICU 68, this constant equaled an instance of NoUnit.
+     *
      * Since ICU 68, this constant is equivalent to MeasureUnit.PERCENT.
      *
-     * @draft ICU 60
+     * @draft ICU 68
      * @provisional This API might change or be removed in a future release.
      */
     public static final MeasureUnit PERCENT = MeasureUnit.PERCENT;
@@ -34,9 +38,11 @@ public final class NoUnit {
     /**
      * Constant for the permille unit, or 1/100 of a base unit.
      *
+     * Prior to ICU 68, this constant equaled an instance of NoUnit.
+     *
      * Since ICU 68, this constant is equivalent to MeasureUnit.PERMILLE.
      *
-     * @draft ICU 60
+     * @draft ICU 68
      * @provisional This API might change or be removed in a future release.
      */
     public static final MeasureUnit PERMILLE = MeasureUnit.PERMILLE;
